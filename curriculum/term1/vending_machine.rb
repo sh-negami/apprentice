1b79f6ef19229107578a4f2e6fb9fe41da489844
sh-negami <shumai229@gmail.com>
2026-07-22 09:17:15 +0900

    desktop

--- curriculum/term1/vending_machine.rb
+++ curriculum/term1/vending_machine.rb
@@ -2,20 +2,26 @@ class VendingMachine
 
   def initialize(m_name)
     @m_name = m_name
-    @coin = false
+    @deposit = 0
+    @cup = 0
   end
   
-  def press_button
-    if @coin
-      @coin = false
-      'cider'
+  def press_button(item)
+    
+    if @deposit >= item.price
+      @deposit -= item.price
+      puts item.name
     end
   end
 
   def deposit_coin(amount)
     if amount == 100
-      @coin = true
+      @deposit += 100
     end
+  end
+
+  def add_cup(num)
+    @cup += num
   end
 
   private
@@ -26,13 +32,33 @@ class VendingMachine
 
 end
 
-vending_machine = VendingMachine.new('サントリー')
-puts vending_machine.press_button
+class Item
 
-vending_machine.deposit_coin(150)
-puts vending_machine.press_button
+  attr_reader :name, :price, :cup
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+    @cup = false
+  end
+end
+
+class Drink < Item
+end
+
+class CupCoffee < Item
+  def initialize
+    @cup = true
+  end
+end
+
+hot_cup_coffee = CupCoffee.new('hot', 100)
+cider = Item.new('cider', 100)
+vending_machine = VendingMachine.new('サントリー')
 
 vending_machine.deposit_coin(100)
-puts vending_machine.press_button
+vending_machine.deposit_coin(100)
+puts vending_machine.press_button(cider)
 
-puts vending_machine.press_manufacturer_name
+puts vending_machine.press_button(hot_cup_coffee)
+vending_machine.add_cup(1)
